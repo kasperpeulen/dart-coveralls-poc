89fca4895ca1296af6d6c3d600ba5af21245a87b
Thanasis Voivodas <thanasis.voivodas@gt.central>
2015-09-18 10:32:38 +0300

    changes run.sh file that it return exit code 1 if the tests have failed

--- test/run.sh
+++ test/run.sh
@@ -19,7 +19,7 @@ echo -e "$results"
 
 # check to see if DumpRenderTree tests
 # fails, since it always returns 0
-if [[ "$results" == *"Some tests failed"* ]]
+if [[ "$results" == *"FAIL"* ]]
 then
   exit 1
 fi
