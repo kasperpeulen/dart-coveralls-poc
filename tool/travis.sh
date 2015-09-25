a669b245c2cfb67692989cdb43bbbdd95abb7e5a
Thanasis Voivodas <thanasis.voivodas@gt.central>
2015-09-25 13:03:49 +0300

    adds a travis.sh file that travis ci will execute on build

---
+++ tool/travis.sh
@@ -0,0 +1,13 @@
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+pub run test -p content-shell
+
+
+if [ "$REPO_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $REPO_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/data_service_test.dart
+fi
