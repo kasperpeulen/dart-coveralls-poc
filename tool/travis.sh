0eee229de4da76e2701027dc595fd4ceb8267121
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-20 01:45:05 +0100

    push

--- tool/travis.sh
+++ tool/travis.sh
@@ -9,10 +9,9 @@ then
   exit 1
 fi
 
-if [ "$REPO_TOKEN" ]; then
+if [ "$COVERALLS_TOKEN" ]; then
   pub global activate dart_coveralls
   pub global run dart_coveralls report \
-    --token $REPO_TOKEN \
     --retry 2 \
     --exclude-test-files \
     test/data_service_test.dart
